6bbcdf020887af19552c958c0a230d34c70af539
d.stolpovskikh <danil.stolpovskikh@gmail.com>
2023-07-26 23:57:28 +0700

    Setup prj to using postgres

--- src/config/settings.py
+++ src/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%qvh$lbnss)x!y^b0kt)m#)=ynm+x4^vl1m289jcp*&a8991v$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
+    'django.contrib.postgres'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neuro',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'postgresql',
+        'PORT': '5432',
     }
 }
 
