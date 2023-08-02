b4f8ffc963cf8be36cd8fd68569ddc3c334cdd06
d.stolpovskikh <danil.stolpovskikh@gmail.com>
2023-08-03 00:25:20 +0700

    feature: create and update in base crud

--- src/common/crud/crud.py
+++ src/common/crud/crud.py
@@ -1,5 +1,6 @@
-from typing import Generic, TypeVar
+from typing import Generic, TypeVar, Union, Dict, Any
 from fastapi import HTTPException
+from fastapi.encoders import jsonable_encoder
 from psycopg2 import errors
 from pydantic import BaseModel as PydanticBaseModel
 from sqlalchemy.exc import IntegrityError
@@ -34,3 +35,29 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
             if isinstance(e.orig, UniqueViolation):
                 raise HTTPException(status_code=401)
             raise e
+
+    def update(
+        self,
+        db: Session,
+        *,
+        db_obj: ModelType,
+        obj_in: Union[UpdateSchemaType, Dict[str, Any]],
+    ) -> ModelType:
+        obj_data = jsonable_encoder(db_obj)
+        if isinstance(obj_in, dict):
+            update_data = obj_in
+        else:
+            update_data = obj_in.dict(exclude_unset=True)
+        for field in obj_data:
+            if field in update_data:
+                setattr(db_obj, field, update_data[field])
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def remove(self, db: Session, *, sid: Any) -> ModelType:
+        obj = db.query(self.model).filter(self.model.sid == sid).first()
+        db.delete(obj)
+        db.commit()
+        return obj
